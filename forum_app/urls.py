73f03562d11a133e3695f9b0ff8159782b2048bc
Pâmela Brasil Gonella <131402720+Pamela-Brasil@users.noreply.github.com>
2025-06-02 18:51:58 -0300

    Update urls.py
    inserindo .html no caminho

--- forum_app/urls.py
+++ forum_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.forum_home, name='forum_home'),
-    path('novo/', views.novo_post, name='criar_postagem'),
+    path('', views.forum_home, name='forum_home.html'),
+    path('novo/', views.novo_post, name='criar_postagem.html'),
 ]
