0d45f742ce37dc591dfbfb0be911da5bb405da35
Pâmela Brasil Gonella <131402720+Pamela-Brasil@users.noreply.github.com>
2025-06-02 19:23:30 -0300

    Update urls.py
    inclusão dos caminhos de urls das pasta de usuaria e empresa

--- delastech/urls.py
+++ delastech/urls.py
@@ -21,4 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('forum/', include('forum_app.urls')),
+    path('user/', include('usuaria/urls')),
+    path('empresa/', include('empresa/urls')),
 ]
