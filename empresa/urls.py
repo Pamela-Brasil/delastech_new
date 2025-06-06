7408862a484abf4f46b619665d6fb0d08d5f758e
Pâmela Brasil Gonella <131402720+Pamela-Brasil@users.noreply.github.com>
2025-06-06 20:20:07 -0300

    inclusão dos endereços

--- empresa/urls.py
+++ empresa/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-
+from .views import EmpresasParceiras, Cadastro, Edicao
 
 urlpatterns = [
+    path('parceiras/', EmpresasParceiras.as_view(), name="empresas_parceiras"),
+    path('cadastro', Cadastro.as_view(), name="nova_parceira"),
+    path('editarempresa', Edicao.as_view(), name="editar_empesa"),
 ]
