a4982ff5dd280f6dda453bab35f4b4064100d372
Pâmela Brasil Gonella <131402720+Pamela-Brasil@users.noreply.github.com>
2025-06-06 20:19:34 -0300

    inclusão das classes

--- empresa/views.py
+++ empresa/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 from .models import Empresa
 from django.http import HttpResponse
 from django.views import View
+from django.views.generic import ListView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from .forms import FormEmpresa
+
 # Create your views here.
 
 
-class Cadastro(View):
-    def get(self, request):
-        return HttpResponse(Empresa)
+class EmpresasParceiras(ListView):
+    model = Empresa
+    template_name = 'empresa/emp-parceiras.html'
+
+class Cadastro(CreateView):
+    model = Empresa
+    form_class = FormEmpresa
+    template_name = 'empresa/nova'
+    success_url = 
+
+class Edicao(UpdateView):
+    model = Empresa
+    form_class = FormEmpresa
+    template_name = 'empresa/edit'
+    success_url = 
