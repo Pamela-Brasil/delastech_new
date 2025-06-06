bf4a4cc131e38e96c67c0b6753c94c7020c20a91
Pâmela Brasil Gonella <131402720+Pamela-Brasil@users.noreply.github.com>
2025-06-06 20:20:29 -0300

    inclusão dos endereços

--- empresa/urls.py
+++ empresa/urls.py
@@ -3,6 +3,6 @@ from .views import EmpresasParceiras, Cadastro, Edicao
 
 urlpatterns = [
     path('parceiras/', EmpresasParceiras.as_view(), name="empresas_parceiras"),
-    path('cadastro', Cadastro.as_view(), name="nova_parceira"),
-    path('editarempresa', Edicao.as_view(), name="editar_empesa"),
+    path('cadastro/', Cadastro.as_view(), name="nova_parceira"),
+    path('editarempresa/', Edicao.as_view(), name="editar_empesa"),
 ]
