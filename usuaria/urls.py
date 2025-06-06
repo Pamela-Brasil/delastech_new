1865b8ba9df99085e1dbc83d16768896c3e12667
Pâmela Brasil Gonella <131402720+Pamela-Brasil@users.noreply.github.com>
2025-06-06 20:11:34 -0300

    inclusão dos endereços

--- usuaria/urls.py
+++ usuaria/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-
 urlpatterns = [
-
+    path('novadela/', CadastroUs.as_view(), name="nova_user"),
+    path('editarela/', EdicaoUs.as_view(), name="edit_user"),
+    path('candidatas/', ListaUsuarias.as_view(), name="candidatas"),
 ]
