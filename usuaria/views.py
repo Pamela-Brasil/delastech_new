7369ae1c47f601dca204c1a0ddf0f6a4724282f1
Pâmela Brasil Gonella <131402720+Pamela-Brasil@users.noreply.github.com>
2025-06-06 20:14:06 -0300

    inclusão das classes CRUD

--- usuaria/views.py
+++ usuaria/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from .models import Usuaria
+from .forms import FormUsuaria
+from django.views.generic import ListView,CreateView,UpdateView
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+class CadastroUs(CreateView):
+    model = Usuaria
+    form_class = FormUsuaria
+    template_name = 'usuaria/novausuaria.html'
+    success_urls = 
+
+
+class EdicaoUs(UpdateView):
+    model = Usuaria
+    form_class = FormUsuaria
+    template_name = 'usuaria/edituser.html'
+    success_url = 
+  
+
+class ListaUsuarias(ListView):  #Nessa class, mais tarde entrará a página de consulta das empresas com filtro
+    model = Usuaria
+    form_class = FormUsuaria
+    template_name = 'usuaria/candidatas.html'
+    
