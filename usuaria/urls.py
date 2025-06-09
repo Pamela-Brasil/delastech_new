3439e1c1663c0bc52cc12730282229bdb5cdbd3c
Pâmela Brasil Gonella <131402720+Pamela-Brasil@users.noreply.github.com>
2025-06-09 18:24:28 -0300

    inclusão url de exclusão

--- usuaria/urls.py
+++ usuaria/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('novadela/', CadastroUs.as_view(), name="nova_user"),
     path('editarela/', EdicaoUs.as_view(), name="edit_user"),
     path('candidatas/', ListaUsuarias.as_view(), name="candidatas"),
+    path('deleta/', Exclusao.as_view(), name="delete_user"),
 ]
