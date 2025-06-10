c0506fece9f7c2c67212d545d874a58eded84b94
Pâmela Brasil Gonella <131402720+Pamela-Brasil@users.noreply.github.com>
2025-06-10 15:48:22 -0300

    correção da ordem

--- empresa/admin.py
+++ empresa/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Empresa
 
-admin.site.register(Empresa)
-
 class EmpresaAdmin(admin.ModelAdmin):
     list_display = ('nome', 'porte', 'cnpj', 'contato')
+
+admin.site.register(Empresa, EmpresaAdmin)
