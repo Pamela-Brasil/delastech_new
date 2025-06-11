ce531ea167e8679aa1d34be615e3ac607be93db3
Pamela-Brasil <131402720+Pamela-Brasil@users.noreply.github.com>
2025-06-11 11:06:29 -0300

    correção de conflits

--- empresa/forms.py
+++ empresa/forms.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django import forms
 from .models import Empresa
 
@@ -6,11 +5,11 @@ class FormEmpresa(forms.ModelForm):
     class Meta:
         model = Empresa
         fields = [
-            'nome'
-            'cnpj'
-            'seguimento'
-            'porte'
-            'contato'
+            'nome',
+            'cnpj',
+            'seguimento',
+            'porte',
+            'contato',
         ]
         widgets = {
             "nome":forms.TextInput(attrs={"class":"form-control","placeholder":"Nome da Empresa"}),
@@ -19,25 +18,3 @@ class FormEmpresa(forms.ModelForm):
             "porte": forms.TextInput(attrs={"class":"form-control","placeholder":"Porte da Empresa"}),
             "contato": forms.EmailInput(attrs={"class":"form-control","placeholder":"E-mail de Contato"})
         }
-=======
-from django import forms
-from .models import Empresa
-
-class FormEmpresa(forms.ModelForm):
-    class Meta:
-        model = Empresa
-        fields = [
-            'nome'
-            'cnpj'
-            'seguimento'
-            'porte'
-            'contato'
-        ]
-        widgets = {
-            "nome":forms.TextInput(attrs={"class":"form-control","placeholder":"Nome da Empresa"}),
-            "cnpj": forms.TextInput(attrs={"class":"form-control","placeholder":"Digite o CNPJ"}),
-            "seguimento": forms.TextInput(attrs={"class":"form-control","placeholder":"Seguimento de Mercado"}),
-            "porte": forms.TextInput(attrs={"class":"form-control","placeholder":"Porte da Empresa"}),
-            "contato": forms.EmailInput(attrs={"class":"form-control","placeholder":"E-mail de Contato"})
-        }
->>>>>>> d79063c23d590f9d216d02ec4f01b8bb8d811ea2
