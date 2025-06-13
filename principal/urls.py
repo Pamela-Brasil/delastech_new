200913f9d1235303a1cf72a309c2348ad18cf304
Pamela-Brasil <131402720+Pamela-Brasil@users.noreply.github.com>
2025-06-13 17:20:39 -0300

    correção header e footer, adição de novas páginas

--- principal/urls.py
+++ principal/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import HomePage
+from .views import HomePage, Sobre, Contato, Servicos
 
 urlpatterns = [
     path('', HomePage, name='home'),
+    path('sobre/', Sobre, name='sobre'),
+    path('contato/', Contato, name='contato'),
+    path('servicos/', Servicos, name='servicos'),
 ]
--- principal/views.py
+++ principal/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def HomePage(request):
     return render(request, 'principal/home.html')
+
+def Sobre(request):
+    return render(request, 'principal/sobre.html')
+
+def Contato(request):
+    return render(request, 'principal/contato.html')
+
+def Servicos(request):
+    return render(request, 'principal/servicos.html')
