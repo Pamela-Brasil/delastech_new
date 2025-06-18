2315618e72adfddc01986637b9d62d113f1ce256
Pamela-Brasil <131402720+Pamela-Brasil@users.noreply.github.com>
2025-06-18 18:01:56 -0300

    correção da página sobre e nossos serviços

--- delastech/settings.py
+++ delastech/settings.py
@@ -130,10 +130,10 @@ DATETIME_INPUT_FORMATS = ['%d/%m/%Y %H:%M:%S', '%d/%m/%Y %H:%M']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "static",
 ]
 
 # Default primary key field type
